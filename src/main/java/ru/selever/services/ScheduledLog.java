a1d8a51d9dd4b8e6c3e7600735ca681a07d434b2
N1kq <53380173+N1kq@users.noreply.github.com>
2024-04-29 19:23:13 +0300

    Added ScheduledLog

---
+++ src/main/java/ru/selever/services/ScheduledLog.java
@@ -0,0 +1,16 @@
+package ru.selever.services;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduledLog {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledLog.class);
+    @Scheduled(cron = "0 * * * * ?", zone = "Europe/Moscow")
+    public void SLog() throws InterruptedException{
+        logger.info("Scheduled task complete");
+    }
+}
