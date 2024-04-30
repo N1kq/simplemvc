86d364b26e2c59b61b103e0dabe091600d1b95e8
N1kq <53380173+N1kq@users.noreply.github.com>
2024-04-30 18:57:49 +0300

    Added Telegram dependency, some tweaks in TelegramScheduler.java

--- src/main/java/ru/selever/SimplemvcApplication.java
+++ src/main/java/ru/selever/SimplemvcApplication.java
@@ -3,11 +3,11 @@ package ru.selever;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SimplemvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimplemvcApplication.class, args);
-	}
-
+        SpringApplication.run(SimplemvcApplication.class, args);
+    }
 }
--- src/main/java/ru/selever/services/TelegramScheduler.java
+++ src/main/java/ru/selever/services/TelegramScheduler.java
@@ -3,14 +3,23 @@ package ru.selever.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Service
 public class TelegramScheduler {
+    @Value("${spring.service.telegramscheduler.botname:'botname'}")
+    public String botname;
+    @Value("${spring.service.telegramscheduler.bottoken:'bottoken'}")
+    public String bottoken;
+
     private static final Logger logger = LoggerFactory.getLogger(TelegramScheduler.class);
     @Scheduled(cron = "0 * * * * ?", zone = "Europe/Moscow")
-    public void SLog() {
+    public void tgbot() {
         logger.info("Scheduled task complete");
     }
+
 }
