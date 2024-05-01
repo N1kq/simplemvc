004e8d4427777beb7d68171d9a2c5b4b9d6851bc
N1kq <53380173+N1kq@users.noreply.github.com>
2024-05-01 19:28:31 +0300

    Bot works

--- src/main/java/ru/selever/bot/TgBot.java
+++ src/main/java/ru/selever/bot/TgBot.java
@@ -1,5 +1,7 @@
 package ru.selever.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -7,18 +9,18 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Component
 public class TgBot extends TelegramLongPollingBot {
 
+    Logger logger = LoggerFactory.getLogger(TgBot.class);
     public TgBot(@Value("${spring.service.telegramscheduler.bottoken}") String botToken){
         super(botToken);
+    }
+    public String getBotUsername(){
+        return botname;
     }
     @Value("${spring.service.telegramscheduler.botname}")
     private String botname;
 
     @Override
     public void onUpdateReceived(Update update){
-
+        logger.info(String.valueOf(update));
     }
-
-    public String getBotUsername(){
-        return botname;
-    };
 }
