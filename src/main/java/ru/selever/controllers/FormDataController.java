a4bc0e12df99486aead61b94e66f917a38c28c02
N1kq <53380173+N1kq@users.noreply.github.com>
2024-05-09 15:30:35 +0300

    DataController, Data model, DataRepository created

---
+++ src/main/java/ru/selever/controllers/FormDataController.java
@@ -0,0 +1,62 @@
+package ru.selever.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.selever.models.Data;
+import ru.selever.repository.DataRepository;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/data")
+public class FormDataController {
+    private static final Logger logger = LoggerFactory.getLogger(FormDataController.class);
+
+    @Autowired
+    private final DataRepository dataRepository;
+
+    @Autowired
+    public FormDataController(DataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
+    //POST data
+    @PostMapping
+    public ResponseEntity createData(@RequestBody Data data) throws URISyntaxException{
+        logger.debug("Пришел Post-запрос на создание записи");
+        Data savedData = dataRepository.save(data);
+        return ResponseEntity.created(new URI("/data/"+ savedData.getMessageId())).body(savedData);
+    }
+
+//    //GET ALL record
+//    @GetMapping
+//    public List<Data> getData(){
+//        logger.info("Пришел Get-запрос на все записи таблицы");
+//        return dataRepository.findAll();
+//    }
+//
+//    //PUT Update Data
+//    @PutMapping("/{id}")
+//    public ResponseEntity updateData(@PathVariable Long messageId, @RequestBody Data data){
+//        logger.debug("Пришел Put-запрос на изменениие записи");
+//        Data currentData = dataRepository.findById(messageId).orElseThrow(RuntimeException::new);
+//        currentData.setMessage(data.getMessage());
+//        currentData.setEditdate(data.getEditdate());
+//        currentData.setName(data.getName());
+//        currentData.seteMail(data.geteMail());
+//        currentData.setPhoneNumber(data.getPhoneNumber());
+//        return ResponseEntity.ok(currentData);
+//
+//    }
+//    //DELETE data
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity deleteData(@PathVariable Long messageId){
+//        logger.debug("Пришел Delete-запрос на удаление записи");
+//        dataRepository.deleteById(messageId);
+//        return ResponseEntity.ok().build();
+//    }
+}
