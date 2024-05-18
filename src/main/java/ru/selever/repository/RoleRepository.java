a491d5243c3d874838c43b81d89d0dd04e298dcd
N1kq <53380173+N1kq@users.noreply.github.com>
2024-05-18 20:59:59 +0300

    Added RoleRepository

---
+++ src/main/java/ru/selever/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package ru.selever.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.selever.models.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
