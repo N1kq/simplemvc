dd25f23d5b8394281e225439cb678f107d5f0c49
N1kq <53380173+N1kq@users.noreply.github.com>
2024-05-18 20:57:41 +0300

    Added Role model

---
+++ src/main/java/ru/selever/models/Role.java
@@ -0,0 +1,40 @@
+package ru.selever.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(Long roleId, String name) {
+        this.roleId = roleId;
+        this.name = name;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
